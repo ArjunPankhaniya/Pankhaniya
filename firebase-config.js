54c6c1f36b7641cefd2a2d4a4c4c730c695679c0
ArjunPankhaniya <ArjunPankhaniya@users.noreply.github.com>
2025-08-24 14:23:48 +0000

    deploy: a9a91c57ca30aed30e758deaa18ee440b9abf3ad

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyBElLKK-Sx4F9dq6lEaQGlAp1noQPTuz_w',
+    authDomain: 'pankhaniyafamilytree.firebaseapp.com',
+    databaseURL: 'https://pankhaniyafamilytree-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'pankhaniyafamilytree',
+    storageBucket: 'pankhaniyafamilytree.firebasestorage.app',
+    messagingSenderId: '607589341613',
+    appId: '1:607589341613:web:d9708b169bbc5287066924',
+    measurementId: 'G-M5SCLDQKT0'
+};
+export { firebaseConfig };
